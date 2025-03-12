97e21e3558c9871f4071038226c5fc219c701195
Yılmaz Ev <yilmaz.ev@outlook.com>
2025-03-12 18:13:24 +0300

    bug-fix: eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Rubik } from "next/font/google"
 import ThemeProvider from "../components/ThemeProvider"
 import "../styles/globals.css"
 
-const rubik = Rubik({ subsets: ["latin"], weight: ["300", "400", "500", "600", "700", "800", "900"] })
+const rubik = Rubik({ subsets: [ "latin" ], weight: [ "300", "400", "500", "600", "700", "800", "900" ] })
 
 export const metadata: Metadata = {
   title: "Yilmaz Ev - Frontend Developer",
