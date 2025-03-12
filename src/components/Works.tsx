77af7cbe086a19604d46e3887067b44d49d286a6
Yılmaz Ev <yilmaz.ev@outlook.com>
2025-03-12 16:56:12 +0300

    enc: works text added to en.json

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,16 +1,19 @@
 "use client"
 
 import { works } from "@/data/works.json"
+import { useTranslations } from "next-intl"
 import Image from "next/image"
 import Link from "next/link"
 import IconMoveRight from "../assets/icons/move-right.svg"
 
 const Works = () => {
+  const t = useTranslations()
+
   return (
     <section id="works" className="flex items-center justify-center py-4">
       <div className="container flex flex-col items-center justify-center pt-16 pb-32">
         <h2 className="text-5xl leading-[1] font-medium text-center w-3/6 mb-10">
-          Selected works
+          {t("works_title")}
         </h2>
         <div className="flex flex-col gap-12 w-full">
           {works.map((work, index) => (
@@ -27,7 +30,7 @@ const Works = () => {
                     title={work.title}
                     className="flex items-center justify-between gap-2 px-5 py-3 hover:bg-neutral-50 hover:dark:bg-neutral-950 border border-neutral-300 dark:border-neutral-800 rounded-full transition-all"
                   >
-                    <span>Open case</span>
+                    <span>{t("works_btn_case")}</span>
                     <IconMoveRight className="size-7" />
                   </Link>
                 </div>
